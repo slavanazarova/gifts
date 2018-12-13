1e58f2a02b69260bcb1c42a63391e5c0b25c08a5
SlavaNazarova <2003nvv@mail.ru>
2018-12-13 19:18:52 +0700

    [+] new details

---
+++ app/src/main/java/ru/future/scientist/gifts/GiftDetails.java
@@ -0,0 +1,14 @@
+package ru.future.scientist.gifts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GiftDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gift_details);
+    }
+}
